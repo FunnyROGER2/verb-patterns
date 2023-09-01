19f826438ae64531c21a3bbef8f1a256b7cfbd48
v.nikitin <ipo.it.cx@mail.ru>
2023-09-02 02:46:21 +0500

    Base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
+import { verbs } from "./verbs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  state = {
+    isOptionsShown: true,
+    currentVerb: null,
+  };
+
+  render() {
+    return (
+      <div className="container h-100 d-flex flex-column">
+        <h1 className="display-1">Verb patterns</h1>
+        <div className="flex-grow-1 d-flex flex-column">
+          <div className="d-flex mb-auto">
+            <h2 className="h1">{this.state.currentVerb?.name}</h2>
+            <span
+              className={
+                "badge align-self-center ms-2 text-bg-" + this.state.result
+              }
+            >
+              {this.state.result ? this.state.currentVerb.type : ""}
+            </span>
+          </div>
+          <div className="mb-3">
+            {this.state.isOptionsShown ? (
+              <div className="btn-group">
+                <button
+                  type="button"
+                  className="btn btn-lg btn-outline-primary"
+                  onClick={this.choseVerb("gerund")}
+                >
+                  Gerund
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-lg btn-outline-primary"
+                  onClick={this.choseVerb("to")}
+                >
+                  To
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-lg btn-outline-primary"
+                  onClick={this.choseVerb("empty")}
+                >
+                  Empty
+                </button>
+              </div>
+            ) : (
+              <button className="btn btn-lg btn-primary" onClick={this.next}>
+                Next
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.showVerb();
+  }
+
+  showVerb = () => {
+    this.setState({
+      currentVerb: this.getVerb(),
+    });
+  };
+
+  getVerbType = (name) => {
+    for (const type in verbs) {
+      if (verbs[type].some((verb) => verb.name === name)) {
+        return type;
+      }
+    }
+    return null;
+  }
+
+  getVerb = () => {
+    const verbsArr = [...verbs.gerund, ...verbs.to, ...verbs.empty];
+    const num = Math.floor(Math.random(1) * verbsArr.length);
+    const verb = [...verbs.gerund, ...verbs.to, ...verbs.empty][num];
+    verb.type = this.getVerbType(verb.name);
+    return verb;
+  };
+
+  choseVerb = (type) => {
+    return () => {
+      this.setState({
+        result: type === this.state.currentVerb.type ? "success" : "danger",
+        isOptionsShown: false
+      });
+    }
+  }
+
+  next = () => {
+    this.showVerb();
+    this.setState({
+      result: null,
+      isOptionsShown: true,
+    });
+  };
 }
 
 export default App;
---
+++ src/verbs.js
@@ -0,0 +1,182 @@
+export const verbs = {
+  gerund: [
+    {
+      name: "admit",
+    },
+    {
+      name: "avoid",
+    },
+    {
+      name: "be worth",
+    },
+    {
+      name: "can't help",
+    },
+    {
+      name: "can't stand",
+    },
+    {
+      name: "carry on",
+    },
+    {
+      name: "give up",
+    },
+    {
+      name: "imagine",
+    },
+    {
+      name: "involve",
+    },
+    {
+      name: "keep (on)",
+    },
+    {
+      name: "look forward to",
+    },
+    {
+      name: "mind",
+    },
+    {
+      name: "miss",
+    },
+    {
+      name: "postpone",
+    },
+    {
+      name: "practise",
+    },
+    {
+      name: "recommend",
+    },
+    {
+      name: "regret",
+    },
+    {
+      name: "risk",
+    },
+    {
+      name: "spend",
+    },
+    {
+      name: "stop",
+    },
+    {
+      name: "suggest",
+    },
+  ],
+  to: [
+    {
+      name: "afford",
+    },
+    {
+      name: "agree",
+    },
+    {
+      name: "appear",
+    },
+    {
+      name: "arrange",
+    },
+    {
+      name: "be able",
+    },
+    {
+      name: "can't wait",
+    },
+    {
+      name: "choose",
+    },
+    {
+      name: "decide",
+    },
+    {
+      name: "deserve",
+    },
+    {
+      name: "expect",
+    },
+    {
+      name: "happen",
+    },
+    {
+      name: "help",
+    },
+    {
+      name: "hesitate",
+    },
+    {
+      name: "hope",
+    },
+    {
+      name: "learn",
+    },
+    {
+      name: "be make",
+    },
+    {
+      name: "manage",
+    },
+    {
+      name: "offer",
+    },
+    {
+      name: "plan",
+    },
+    {
+      name: "pretend",
+    },
+    {
+      name: "promise",
+    },
+    {
+      name: "refuse",
+    },
+    {
+      name: "seem",
+    },
+    {
+      name: "teach",
+    },
+    {
+      name: "tend",
+    },
+    {
+      name: "threaten",
+    },
+    {
+      name: "want",
+    },
+    {
+      name: "would like",
+    },
+  ],
+  empty: [
+    {
+      name: "can",
+    },
+    {
+      name: "had better",
+    },
+    {
+      name: "let",
+    },
+    {
+      name: "make",
+    },
+    {
+      name: "may",
+    },
+    {
+      name: "might",
+    },
+    {
+      name: "must",
+    },
+    {
+      name: "should",
+    },
+    {
+      name: "would rather",
+    },
+  ],
+};

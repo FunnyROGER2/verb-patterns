c32511c778e299cb531077649d73cd01621b1b11
v.nikitin <ipo.it.cx@mail.ru>
2023-09-05 03:51:21 +0500

    Добавлен общий список с навигацией

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { PureComponent } from "react";
-import { verbs } from "./verbs";
-
-const renderHTML = (rawHTML) =>
-  React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+import {
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Single from "./Single";
+import List from "./List";
 
 class App extends PureComponent {
   state = {
@@ -14,103 +17,25 @@ class App extends PureComponent {
     return (
       <div className="container h-100 d-flex flex-column">
         <h1 className="display-1 my-2">Verb patterns</h1>
-        <div className="flex-grow-1 d-flex flex-column">
-          <div className="d-flex mb-auto">
-            <h2 className="h1">{this.state.currentVerb?.name}</h2>
-            <span
-              className={
-                "badge align-self-center ms-2 text-bg-" + this.state.result
-              }
-            >
-              {this.state.result ? this.state.currentVerb.type : ""}
-            </span>
-          </div>
-          {this.state.result ? (
-            <div className="example">
-              {renderHTML(this.state.currentVerb?.example)}
-            </div>
-          ) : (
-            ""
-          )}
-          <div className="mb-3 mt-auto">
-            {this.state.isOptionsShown ? (
-              <div className="btn-group">
-                <button
-                  type="button"
-                  className="btn btn-lg btn-outline-primary"
-                  onClick={this.choseVerb("gerund")}
-                >
-                  Gerund
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-lg btn-outline-primary"
-                  onClick={this.choseVerb("to")}
-                >
-                  To
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-lg btn-outline-primary"
-                  onClick={this.choseVerb("empty")}
-                >
-                  Empty
-                </button>
-              </div>
-            ) : (
-              <button className="btn btn-lg btn-primary" onClick={this.next}>
-                Next
-              </button>
-            )}
-          </div>
-        </div>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <NavLink className="nav-link" to={`/`}>
+              Quiz
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to={`/list`}>
+              List
+            </NavLink>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="*" element={<Single />} />
+          <Route path="list" element={<List />} />
+        </Routes>
       </div>
     );
   }
-
-  componentDidMount() {
-    this.showVerb();
-  }
-
-  showVerb = () => {
-    this.setState({
-      currentVerb: this.getVerb(),
-    });
-  };
-
-  getVerbType = (name) => {
-    for (const type in verbs) {
-      if (verbs[type].some((verb) => verb.name === name)) {
-        return type;
-      }
-    }
-    return null;
-  }
-
-  getVerb = () => {
-    const verbsArr = [...verbs.gerund, ...verbs.to, ...verbs.empty];
-    const num = Math.floor(Math.random(1) * verbsArr.length);
-    const verb = [...verbs.gerund, ...verbs.to, ...verbs.empty][num];
-    verb.type = this.getVerbType(verb.name);
-    return verb;
-  };
-
-  choseVerb = (type) => {
-    return () => {
-      this.setState({
-        result: type === this.state.currentVerb.type ? "success" : "danger",
-        isOptionsShown: false
-      });
-    }
-  }
-
-  next = () => {
-    this.showVerb();
-    this.setState({
-      result: null,
-      isOptionsShown: true,
-    });
-  };
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,138 @@
+import React, { PureComponent } from "react";
+import { verbs } from "./verbs";
+
+const renderHTML = (rawHTML) =>
+  React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+
+class List extends PureComponent {
+  state = {
+    filter: "",
+    currentVerb: null,
+  };
+
+  getTheBiggestVerbType = () => {
+    let largestKey = null;
+    let largestLength = -1;
+
+    for (const key in verbs) {
+      if (Array.isArray(verbs[key]) && verbs[key].length > largestLength) {
+        largestKey = key;
+        largestLength = verbs[key].length;
+      }
+    }
+
+    return largestKey;
+  };
+
+  filteredVerbs = {}
+
+  getFilteredVerbs = () => {
+    const filteredVerbs = {};
+
+    for (const category in verbs) {
+      const categoryVerbs = verbs[category].filter((verb) =>
+        verb.name.includes(this.state.filter)
+      );
+
+      if (categoryVerbs.length > 0) {
+        filteredVerbs[category] = categoryVerbs;
+      } else {
+        filteredVerbs[category] = [];
+      }
+    }
+
+    return filteredVerbs;
+  };
+
+  getTable = (filteredVerbs) => {
+    return filteredVerbs[this.getTheBiggestVerbType()].map((g, index) => (
+      <tr key={index}>
+        {Object.keys(filteredVerbs).map((verbType, typeIndex) => (
+          <td key={typeIndex}>
+            {filteredVerbs[verbType][index] ? (
+              <div className="d-flex">
+                <button
+                  className="btn btn-outline-primary btn-sm m-auto"
+                  onClick={this.clickVerb(filteredVerbs[verbType][index])}
+                >
+                  {filteredVerbs[verbType][index]?.name}
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </td>
+        ))}
+      </tr>
+    ));
+  }
+
+  render() {
+    return (
+      <div className="list-wrapper d-flex flex-column">
+        <div className="p-1 bg-body">
+          <form className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Filter"
+              onInput={this.setFilter}
+            />
+            <button
+              className="btn btn-outline-primary"
+              type="reset"
+              onClick={this.reset}
+            >
+              Clear
+            </button>
+          </form>
+        </div>
+        <div className="flex-grow-1 d-flex flex-column overflow-auto mb-2">
+          <table className="table list-table">
+            <thead className="table-light list-table__head">
+              <tr>
+                {Object.keys(verbs).map((verbType, typeIndex) => (
+                  <th scope="col" key={typeIndex} className="text-center">
+                    {verbType}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>{this.getTable(this.getFilteredVerbs())}</tbody>
+          </table>
+        </div>
+        {this.state.currentVerb && (
+          <div className="pb-2">
+            <div className="card text-bg-primary">
+              <div className="card-body py-2">
+                {renderHTML(this.state.currentVerb?.example)}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  clickVerb = (verb) => {
+    return () => {
+      this.setState({
+        currentVerb: verb,
+      });
+    };
+  };
+
+  setFilter = (e) => {
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      filter: ""
+    });
+  }
+}
+
+export default List;
